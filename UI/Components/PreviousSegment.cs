924bfdd54edfe5d70d96d12c47dd78570977219d
Jesse Jones <jesseoj98@gmail.com>
2022-01-17 14:14:18 -0600

    Remove use of unnecessary drop decimal property

--- UI/Components/PreviousSegment.cs
+++ UI/Components/PreviousSegment.cs
@@ -162,7 +162,7 @@ namespace LiveSplit.UI.Components
             DeltaFormatter.Accuracy = Settings.DeltaAccuracy;
             DeltaFormatter.DropDecimals = Settings.DropDecimals;
             TimeSaveFormatter.Accuracy = Settings.TimeSaveAccuracy;
-            TimeSaveFormatter.DropDecimalsPossibleTimeSave = Settings.DropDecimalsPossibleTimeSave;
+            TimeSaveFormatter.DropDecimals = Settings.DropDecimalsPossibleTimeSave;
 
             TimeSpan? timeChange = null;
             TimeSpan? timeSave = null;
